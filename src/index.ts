3ebd2bb967a280aff837219eda0e8fdef9f8ce3e
EduBarrros <eduardodeoliveirabarros@gmail.com>
2024-02-01 19:19:16 -0300

    CONFIG: Initializing eslint and editorConfig configuration

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import { server } from "./server/server";
 
-server.listen(3333, () => console.log('O servidor esta rodando na porta 3333'))
+server.listen(3333, () => console.log("O servidor esta rodando na porta 3333"));
